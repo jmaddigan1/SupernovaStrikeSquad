57dd09e7649b058930c82227cdb0758d6818889c
zaclink97 <51802916+zaclink97@users.noreply.github.com>
2022-01-18 20:09:27 -0500

    zac kindy

---
+++ Supernova Strike Squad v2.0 URP/Assets/DoorOpen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    [SerializeField] private Animator myAnimationController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            myAnimationController.SetBool("OpenDoor", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("OpenDoor", false);
+        }
+    }
+}
---
+++ Supernova Strike Squad v2.0 URP/Assets/ElevatorPad.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorPad : MonoBehaviour
+{
+    public float timer;
+    public Vector3 tempY;
+    public Vector3 moveHeight;
+    public Vector3 topHeight;
+    bool atTop;
+    bool atBottom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 3;
+        moveHeight = new Vector3(0,10, 0);
+        tempY = this.transform.position;
+        topHeight = tempY + moveHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (this.transform.position.y >= tempY.y && atTop == false)
+        { 
+            if (timer <= 0)
+            {
+                this.transform.Translate(0,10f* 0.5f * Time.deltaTime,0, Space.World);
+
+                if (this.transform.position.y > topHeight.y)
+                {
+                    this.transform.position = topHeight;
+                    atTop = true;
+                    atBottom = false;
+                    timer = 3;
+                }
+            }
+        }
+        else if (this.transform.position.y <= topHeight.y && atBottom == false)
+        {
+            if (timer <= 0)
+            {
+                this.transform.Translate(0, -10f* 0.5f * Time.deltaTime, 0, Space.World);
+
+                if (this.transform.position.y < tempY.y)
+                {
+                    this.transform.position = tempY;
+                    atTop = false;
+                    atBottom = true;
+                    timer = 3;
+                }
+            }
+
+        }
+    }
+}
