c5f1b0c8f990824677c34494d8572cba404752fc
Alex Hicks <alexhicks@live.ca>
2022-03-18 20:33:33 -0400

    [Bootstrap] Setting up ApplicationManager and starting transition into the MainMenu scene

---
+++ Supernova Strike Squad v2.0 URP/Assets/Scripts/Managers/ApplicationManager.cs
@@ -0,0 +1,36 @@
+﻿using Supernova.Utilities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Supernova.Managers {
+
+	public class ApplicationManager : Singleton<ApplicationManager> {
+
+		#region Serialized Fields
+
+		[SerializeField]
+		private string startingScene;
+
+		#endregion
+
+
+		#region MonoBehaviour
+
+		public void Start() {
+			SceneManager.Instance.LoadScene(startingScene, sceneName => {}, false);
+		}
+
+		#endregion
+		
+
+		#region State
+
+		public enum State {
+			None,
+			MainMenu
+		}
+
+		#endregion
+		
+	}
+}
