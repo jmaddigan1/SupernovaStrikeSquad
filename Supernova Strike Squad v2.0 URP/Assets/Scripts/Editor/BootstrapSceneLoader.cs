9994525eb926ec1c72931cd74f0e8fc20bebfdb1
Alex Hicks <alexhicks@live.ca>
2022-03-21 22:20:45 -0400

    Adding MenuItem to quickly navigate back to the Bootstrap scene for testing

---
+++ Supernova Strike Squad v2.0 URP/Assets/Scripts/Editor/BootstrapSceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using Supernova.Utilities;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Supernova.Editor {
+	public class BootstrapSceneLoader {
+
+		[MenuItem("Supernova/Bootstrap")]
+		public static void OpenBootstrapScene() {
+			EditorSceneManager.OpenScene(SceneList.BOOTSTRAP);
+		}
+
+	}
+}
