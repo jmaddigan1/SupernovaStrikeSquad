12899e90227e0655db48021becbd05cb069fc6e7
Alex Hicks <alexhicks@live.ca>
2022-03-22 23:30:48 -0400

    [Views] Updating MainMenuView with a template animation

--- Supernova Strike Squad v2.0 URP/Assets/Code/UI/Views/MainMenuView.cs
+++ Supernova Strike Squad v2.0 URP/Assets/Code/UI/Views/MainMenuView.cs
@@ -1,13 +1,32 @@
 ﻿using Supernova.Nodes.ViewGraph;
 using Supernova.Utilities.Attributes;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Supernova.UI.Views {
 	public class MainMenuView : ViewBase {
 
+		#region Serialized Fields
+
+		[SerializeField]
+		private Button startGameButton;
+
+		[SerializeField]
+		private Button settingsButton;
+
+		#endregion
+		
+		
 		#region XNode
 
 		[ViewOutput]
 		public EmptyNode StartGameNode {
+			get;
+			set;
+		}
+		
+		[ViewOutput]
+		public EmptyNode SettingsNode {
 			get;
 			set;
 		}
@@ -16,11 +35,13 @@ namespace Supernova.UI.Views {
 		
 		
 		public override void CleanupButtonListeners() {
-			
+			startGameButton.onClick.RemoveAllListeners();
+			settingsButton.onClick.RemoveAllListeners();
 		}
 
 		public override void SetupButtonListeners() {
-			
+			startGameButton.onClick.AddListener(() => OnProcessNode("StartGameNode"));
+			settingsButton.onClick.AddListener(() => OnProcessNode("SettingsNode"));
 		}
 	}
 }
