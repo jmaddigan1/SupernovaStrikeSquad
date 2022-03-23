ab3e4e81127673ce20e2464ad92c448372c5b43e
Alex Hicks <alexhicks@live.ca>
2022-03-22 23:31:02 -0400

    [Views] Adding some new nodes for the view graph

---
+++ Supernova Strike Squad v2.0 URP/Assets/Code/Nodes/ViewGraph/Process/RunInGameNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+namespace Supernova.Nodes.ViewGraph.Process {
+	public class RunInGameNode : LogicNode {
+		
+		[Output]
+		public EmptyNode exit;
+		
+		//TODO: Fill out with possible UI routes and graphs we can take in game
+		
+		public override IEnumerator ProcessNode() {
+			this.State = NodeState.Running;
+			
+			yield return RunPort("exit");
+		}
+	}
+}
