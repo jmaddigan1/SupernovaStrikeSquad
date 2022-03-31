644f0b9492a67a0832d8c87afce8bb1b29499a44
Alex Hicks <alexhicks@live.ca>
2022-03-30 21:14:05 -0400

    Added in a new Lobby view

---
+++ Supernova Strike Squad v2.0 URP/Assets/Code/UI/Views/Elements/LobbyViewPlayerElement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Supernova.UI.Views.Elements {
+	public class LobbyViewPlayerElement : MonoBehaviour {
+
+		[SerializeField]
+		private Image avatarImage;
+
+		[SerializeField]
+		private TextMeshProUGUI usernameText;
+
+		[SerializeField]
+		private GameObject hostImageGameObject;
+
+		[SerializeField]
+		private Button kickButton;
+
+		public Action KickAction {
+			get;
+			set;
+		}
+
+		public void Initialize(Sprite avatar, string username, bool isSelf, bool isHost = false) {
+			avatarImage.sprite = avatar;
+			usernameText.text = username;
+			hostImageGameObject.SetActive(isHost);
+			kickButton.onClick.RemoveAllListeners();
+			kickButton.gameObject.SetActive(!isSelf && isHost);
+
+			if (isHost) {
+				kickButton.onClick.AddListener(DispatchKickAction);	
+			}
+		}
+
+		private void DispatchKickAction() {
+			this.KickAction?.Invoke();
+		}
+
+	}
+}
---
+++ Supernova Strike Squad v2.0 URP/Assets/Code/UI/Views/LobbyView.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using Mirror.Examples.Chat;
+using Supernova.Managers;
+using Supernova.Networking;
+using Supernova.Nodes.ViewGraph;
+using Supernova.UI.Views.Elements;
+using Supernova.Utilities.Attributes;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Supernova.UI.Views {
+	public class LobbyView : ViewBase {
+
+		#region Serialized Fields
+
+		[SerializeField]
+		private Button returnButton;
+
+		[Header("Players")]
+		[SerializeField]
+		private List<LobbyViewPlayerElement> players;
+
+		#endregion
+		
+		
+		#region XNode
+
+		[ViewOutput]
+		public EmptyNode ReturnToGameNode {
+			get;
+			set;
+		}
+
+		#endregion
+		
+		
+		#region Public Overrides
+
+		public override void CleanupButtonListeners() {
+			returnButton.onClick.RemoveAllListeners();
+		}
+
+		public override void SetupButtonListeners() {
+			returnButton.onClick.AddListener(() => OnProcessNode("ReturnToGameNode"));
+		}
+
+		public override void SetupUserInterface() {
+			int numberOfPlayersInGame = CustomNetworkManager.singleton.NumberOfPlayerInGame;
+			for (int i = 0; i < players.Count; i++) {
+				bool isShowing = i < numberOfPlayersInGame;
+				players[i].gameObject.SetActive(isShowing);
+				if (isShowing) {
+					players[i].KickAction = OnKickAction;
+
+					//TODO: Correct this for all players in the game and correct host user
+					if (i == 0) {
+						players[i].Initialize(AccountManager.Instance.AvatarImage, AccountManager.Instance.Username, true, true);
+					} else {
+						players[i].Initialize(null, "Connected User", false);
+					}
+				}
+			}
+		}
+
+		#endregion
+
+
+		#region MonoBehaviour
+
+		public void Update() {
+			if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.F2)) {
+				OnProcessNode("ReturnToGameNode");
+			}
+		}
+
+		#endregion
+
+
+		#region Private Functions
+
+		private void OnKickAction() {
+			//TODO: Implement kick functionality
+		}
+
+		#endregion
+		
+		
+	}
+}
